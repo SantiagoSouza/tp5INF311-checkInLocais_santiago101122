6883ff2b176fb6d475418b91544845f2ebe97f23
santiagosouza <santiagogaudencio18@gmail.com>
2019-06-27 23:54:38 -0300

    Criando projeto e fazendo primeira interface

---
+++ app/src/main/java/br/nom/souza/santiago/checkinlocais/MainActivity.java
@@ -0,0 +1,14 @@
+package br.nom.souza.santiago.checkinlocais;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
